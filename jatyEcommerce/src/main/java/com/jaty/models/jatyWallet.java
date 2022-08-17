f5f514cdb5c7a37550c9901a0a695b8914afd335
1jestor1 <jtorres122436@gmail.com>
2022-08-16 19:00:32 -0500

    refactored jatyWallet to work with Spring ORM

--- jatyEcommerce/src/main/java/com/jaty/models/jatyWallet.java
+++ jatyEcommerce/src/main/java/com/jaty/models/jatyWallet.java
@@ -1,19 +1,34 @@
 package com.jaty.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
 public class jatyWallet {
 	
 	/**
 	 * Unique identifier for internal use.
 	 */
+	@Id
+	@Column(name="id")
+	@GeneratedValue(generator = "jatyWallet_id_seq", strategy = GenerationType.AUTO)
+	@SequenceGenerator(allocationSize=1, name="jatyWallet_id_seq")
 	private int id;
 	/**
 	 * Reference to the jatyAccount id that owns an instance 
 	 * of  jatyWallet.
 	 */
-	private int accountId;
+	@ManyToOne
+	@JoinColumn(name="accountID")
+	private jatyAccount accountId;
 	/**
 	 * How much money is available for use in an jatyAccount.
 	 */
+	@Column(name="balance")
 	private double balance;
 	
 	//constructors for jatyWallet
@@ -27,7 +42,7 @@ public class jatyWallet {
 	 * @param accountId
 	 * @param balance
 	 */
-	public jatyWallet(int id, int accountId, double balance) {
+	public jatyWallet(int id, jatyAccount accountId, double balance) {
 		this.setId(id);
 		this.setAccountId(accountId);
 		this.setBalance(balance);
@@ -39,7 +54,7 @@ public class jatyWallet {
 	 * @param accountId
 	 * @param balance
 	 */
-	public jatyWallet(int accountId, double balance) {
+	public jatyWallet(jatyAccount accountId, double balance) {
 		this.accountId=accountId;
 		this.balance=balance;
 	}
@@ -51,10 +66,10 @@ public class jatyWallet {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getAccountId() {
+	public jatyAccount getAccountId() {
 		return accountId;
 	}
-	public void setAccountId(int accountId) {
+	public void setAccountId(jatyAccount accountId) {
 		this.accountId = accountId;
 	}
 	public double getBalance() {
