9c9e2f04fedd64a2d1604ab8f8d3b2d9f1cbed5a
1jestor1 <jtorres122436@gmail.com>
2022-08-25 09:17:09 -0500

    added cross origin to review controller

--- JATY/src/main/java/com/jaty/controller/ReviewController.java
+++ JATY/src/main/java/com/jaty/controller/ReviewController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,11 +32,13 @@ public class ReviewController {
 		this.reviewService.save(review);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(path="/{productid}/add", consumes=MediaType.APPLICATION_JSON_VALUE)
 	public String addReviewToProduct(@PathVariable int productid, @RequestBody Review review, HttpServletRequest request) {
 		return this.reviewService.createReviewForProduct(productid, review, request);
 	}
 	
+	@CrossOrigin
 	@RequestMapping(path="/{productid}/seeall")
 	public List<Review> seeAllReviewsForProduct(@PathVariable int productid){
 		return this.reviewService.findAllReviewsByProductId(productid);
